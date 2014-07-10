1bf7ddba8c1cc0c56a4919da89f0ab7037ca341f
Mark <hustada80@gmail.com>
2014-07-10 16:12:24 -0500

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710145432) do
+ActiveRecord::Schema.define(version: 20140710201708) do
+
+  create_table "books", force: true do |t|
+    t.string   "title"
+    t.text     "summary"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.text     "text"
+    t.string   "author"
+    t.integer  "book_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
