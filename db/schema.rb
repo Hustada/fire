4aec90f95b405cadf230cfb42cbbf5be437fa521
Mark <hustada80@gmail.com>
2014-07-29 23:30:56 -0500

    added join or start buttons for logged in user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140729005452) do
+ActiveRecord::Schema.define(version: 20140730032123) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20140729005452) do
   create_table "pits", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "topic"
+    t.string   "image"
+    t.string   "video_url"
   end
 
   create_table "users", force: true do |t|
