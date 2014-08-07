597eafcc6997698676dd136c15a05cb1e40ab61d
Mark <hustada80@gmail.com>
2014-08-07 15:16:37 -0500

    revamped homepage, new header

--- app/controllers/pits_controller.rb
+++ app/controllers/pits_controller.rb
@@ -8,12 +8,26 @@ def index
 end
 
 def create
+  @pit = Pit.new(pit_params)
+    
+      if @pit.save
+      redirect_to @pit
+    else
+      render 'new'
+    end
 end
 
 def edit
 end
 
 def update
+end
+
+
+private
+
+def pit_params
+    params.require(:pit).permit(:topic, :summary, :image, :video_url)
 end
 
 end
